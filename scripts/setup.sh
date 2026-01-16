395268ad49e89d7be5a7e394f8070f4e14b21958
alex-marumo <alexmarumo16@gmail.com>
2026-01-16 16:39:15 +0200

    Fix: totally replace the whole repo with the new setup

--- scripts/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# setup.sh - symlink your configs to home with stow
-
-for dir in .config/*; do
-  stow -v -t "$HOME" "$(basename "$dir")"
-done
-
-echo "Configs stowed. Ready to rock your Hyprland rice."
